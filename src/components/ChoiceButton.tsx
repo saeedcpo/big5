114acc31d80caeafbd056151377cc7fa1dd2d109
saeedcpo <40629665+saeedcpo@users.noreply.github.com>
2024-10-07 10:07:21 +0330

    Update ChoiceButton.tsx

--- src/components/ChoiceButton.tsx
+++ src/components/ChoiceButton.tsx
@@ -3,11 +3,13 @@ import { Choice } from '../types';
 interface Props {
   choice: Choice;
   onClick: (choice: Choice) => void;
+  keyboardShortcut: number;
 }
 
 export function ChoiceButton(props: Props) {
   return (
     <button className='w-full md:w-auto btn btn-light' onClick={(e) => {props.onClick(props.choice)}}>
+      <span className="mr-2">{props.keyboardShortcut}:</span>
       {props.choice.text}
     </button>
   );
